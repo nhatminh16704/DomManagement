d85eb4036aa25494f1419c5c66342a11158910ef
đặng việt hùng <dangviethung213@gmail.com>
2025-03-04 23:08:49 +0700

    no

--- src/app/(menu)/announcements/page.tsx
+++ src/app/(menu)/announcements/page.tsx
@@ -71,7 +71,7 @@ export default function Announcements() {
   
 
   return (
-    <div>
+    <div className="flex flex-col h-full">
       <div className="flex justify-between items-center mb-5">
         <h1 className="text-2xl font-semibold text-gray-800 mb-6">Notification</h1>
         <div className="hidden md:flex items-center gap-2 text-sm     rounded-full ring-gray-300 px-2">
@@ -101,18 +101,20 @@ export default function Announcements() {
 
         </div>
       </div>
-      <ul className="space-y-2">
-        {notifications.map((item) => (
-          <li
-            key={item.id}
-            className="flex justify-between items-center border-b border-gray-600 pb-2"
-            onClick={() => navigateToDetailPage(item.id)}
-          >
-            <span className="text-blue-400">▶ {item.title}</span>
-            <span className="text-sm text-gray-400">{item.created_date.toLocaleDateString()}</span>
-          </li>
-        ))}
-      </ul>
+      <div className="flex-grow max-h-[500px] overflow-y-auto border border-gray-600 rounded-md p-2">
+        <ul className="space-y-2">
+          {notifications.map((item) => (
+            <li
+              key={item.id}
+              className="flex justify-between items-center border-b border-gray-600 pb-2 cursor-pointer"
+              onClick={() => navigateToDetailPage(item.id)}
+            >
+              <span className="text-blue-400">▶ {item.title}</span>
+              <span className="text-sm text-gray-400">{item.created_date.toLocaleDateString()}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
 
     </div>
   )
